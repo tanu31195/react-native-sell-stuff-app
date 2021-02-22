f34c4dd04bb7858c92eaeec7c2f0cce15f820bf7
tanu31195 <11332345+tanu31195@users.noreply.github.com>
2021-02-22 23:45:37 +0530

    Added FormField component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -4,9 +4,8 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
 import AppScreen from '../components/AppScreen';
-import ErrorMessage from '../components/ErrorMessage';
+import AppFormField from '../components/AppFormField';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label('Email'),
@@ -29,28 +28,24 @@ export default function LoginScreen() {
             >
                 {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
                     <>
-                        <AppTextInput
+                        <AppFormField
                             autoCapitalize='none'
                             autoCorrect={false}
                             icon='email'
                             keyboardType='email-address'
-                            onBlur={() => setFieldTouched('email')}
-                            onChangeText={handleChange('email')}
+                            name='email'
                             placeholder='Email'
                             textContentType='emailAddress'
                         />
-                        <ErrorMessage error={errors.email} visible={touched.email}/>
-                        <AppTextInput
+                        <AppFormField
                             autoCapitalize='none'
                             autoCorrect={false}
                             icon='lock'
-                            onBlur={() => setFieldTouched('password')}
-                            onChangeText={handleChange('password')}
+                            name='password'
                             placeholder='Password'
                             secureTextEntry
                             textContentType='password'
                         />
-                        <ErrorMessage error={errors.password} visible={touched.password}/>
                         <AppButton onPress={handleSubmit} title='Login' />
                     </>
                 )}
